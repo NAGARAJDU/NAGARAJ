a55085992ff4f9003eea965cb09801457365e907
NAGARAJ <nagarajumatar@gmail.com>
2025-03-13 19:30:11 +0530

    files uploded

--- 11-MAR-2025/studentcrud/studentcrud/src/main/java/com/example/studentcrud/repository/StudentRepository.java
+++ 11-MAR-2025/studentcrud/studentcrud/src/main/java/com/example/studentcrud/repository/StudentRepository.java
@@ -1,11 +1,6 @@
 package com.example.studentcrud.repository;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.studentcrud.model.Student;
 
-
-@Repository
-public interface StudentRepository extends 
-JpaRepository<Student, Long> {
-
+public interface StudentRepository extends JpaRepository<Student, Long>  {
 }
